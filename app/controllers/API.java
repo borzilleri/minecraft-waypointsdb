ff90581e646d24b5e7d7589cefb7dfadcc8b5231
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2013-06-24 13:35:13 -0700

    Implements API#buildFile
    buildFile assumes the request body is a JSON array of string point ids. For
    each point specified in this array, it adds its point file entry to a string,
    which is Cached for a brief time using a simple hash as a cache key.
    
    This hash is returned in the response body for use in downloading the points
    file.

--- app/controllers/API.java
+++ app/controllers/API.java
@@ -1,13 +1,19 @@
 package controllers;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hashing;
 import io.rampant.minecraft.wpdb.dao.PointDAO;
 import models.Point;
+import org.codehaus.jackson.JsonNode;
+import play.cache.Cache;
 import play.data.Form;
 import play.libs.Json;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Inject;
+import java.util.Iterator;
 
 public class API extends Controller {
 	private PointDAO dao;
@@ -54,7 +60,23 @@ public class API extends Controller {
 		return ok();
 	}
 
+	@BodyParser.Of(BodyParser.Json.class)
 	public Result buildFile() {
-		return ok();
+		Iterator<JsonNode> iterator = request().body().asJson().getElements();
+		StringBuilder sb = new StringBuilder();
+
+		while( iterator.hasNext() ) {
+			Point p = dao.get(iterator.next().asText());
+			if( null == p ) {
+				continue;
+			}
+			sb.append(p.toPointString());
+		}
+
+		String pointsFile = sb.toString();
+		String id = Hashing.goodFastHash(32).hashString(pointsFile, Charsets.UTF_8).toString();
+
+		Cache.set("point_" + id, pointsFile, 60*5);
+		return ok(id);
 	}
 }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import com.google.common.base.Strings;
+import play.Logger;
+import play.cache.Cache;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -10,6 +13,14 @@ public class Application extends Controller {
 	}
 
 	public Result download(String file) {
-		return ok();
+		String pointsFile = (String) Cache.get("points_" + file);
+		if( Strings.isNullOrEmpty(pointsFile) ) {
+			return notFound();
+		}
+		String fileName = "world.points";
+
+		response().setContentType("application/x-download");
+		response().setHeader("Content-disposition", "attachment; filename=" + fileName);
+		return ok(pointsFile);
 	}
 }
