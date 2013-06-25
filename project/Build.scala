c1e004f941e1a5fe088863258a6fee38f3515c7f
Jonathan Borzilleri <jborzilleri@intrepidls.com>
2013-06-25 16:23:40 -0700

    Update version to 2.0-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "waypointsdb"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "2.0-SNAPSHOT"
 
   val appDependencies = Seq(
     javaCore, javaJdbc, javaEbean,
